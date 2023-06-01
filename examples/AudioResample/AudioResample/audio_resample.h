752be00bfa65d5a254ab3e2e4bc41799001f65d4
e7e26e5 <abc@xyz.com>
2023-06-01 18:54:55 +0800

    文件重命名

--- examples/AudioResample/AudioResample/audio_resample.h
+++ examples/AudioResample/AudioResample/audio_resample.h
@@ -1,5 +1,5 @@
-#ifndef test_h
-#define test_h
+#ifndef audio_resample_h
+#define audio_resample_h
 
 #include <stdio.h>
 #include "libavutil/avutil.h"
@@ -11,4 +11,4 @@
 void set_status(int status);
 void rec_audio(void);
 
-#endif /* test_h */
+#endif /* audio_resample_h */
