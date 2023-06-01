c966e8b8e4956339c4a7ec97ba6570209f25d3b0
e7e26e5 <abc@xyz.com>
2023-06-01 18:20:10 +0800

    优化

--- examples/AudioRecord/AudioResample/test.c
+++ examples/AudioRecord/AudioResample/test.c
@@ -6,55 +6,53 @@ void set_status(int status) {
     rec_status = status;
 }
 
-void rec_audio() {
+void rec_audio(void) {
+    int ret = 0;
+    char error[1024] = {0,};
+    AVInputFormat *ifmt = NULL;
+    AVFormatContext *ifmt_ctx = NULL;
+    AVDictionary *options = NULL;
+    // ffmpeg -f avfoundation -list_devices true -i ""
+    // [[video device]:[audio device]]
+    const char *device_desc = ":default";
+    AVPacket pkt;
+    FILE *outfile = NULL;
+    
     rec_status = 1;
 
     av_log_set_level(AV_LOG_DEBUG);
 
     avdevice_register_all();
 
-    AVFormatContext *fmt_ctx = NULL;
+    ifmt = av_find_input_format("avfoundation");
 
-    // ffmpeg -f avfoundation -list_devices true -i ""
-    // [[video device]:[audio device]]
-    char *device = ":2";
-
-    AVInputFormat *in_fmt = av_find_input_format("avfoundation");
-
-    AVDictionary *options = NULL;
-
-    int ret = 0;
-    char error[1024] = {0,};
-
-    if ((ret = avformat_open_input(&fmt_ctx, device, in_fmt, &options)) < 0) {
+    ret = avformat_open_input(&ifmt_ctx, device_desc, ifmt, &options);
+    if (ret < 0) {
         av_strerror(ret, error, 1024);
         fprintf(stderr, "Failed to open audio device, [%d] %s\n", ret, error);
         return;
     }
 
-    AVPacket pkt;
-
     // create file
-    FILE *out_file = fopen("/Users/hyh/Downloads/audio.pcm", "wb+");
+    outfile = fopen("/Users/hyh/Downloads/audio.pcm", "wb+");
 
     // read data from device
-    while (av_read_frame(fmt_ctx, &pkt) == 0 &&
+    while (av_read_frame(ifmt_ctx, &pkt) == 0 &&
             rec_status) {
         av_log(NULL, AV_LOG_INFO,
                 "packet size is %d (%p)\n",
                 pkt.size, (void *) pkt.data);
 
         // write file
-        fwrite(pkt.data, 1, (size_t) pkt.size, out_file);
-        fflush(out_file);
-
+        fwrite(pkt.data, 1, (size_t) pkt.size, outfile);
+        fflush(outfile);
         av_packet_unref(&pkt);
     }
 
-    fclose(out_file);
+    fclose(outfile);
 
     // close device and release ctx
-    avformat_close_input(&fmt_ctx);
+    avformat_close_input(&ifmt_ctx);
 
     av_log(NULL, AV_LOG_DEBUG, "finish!\n");
 }
